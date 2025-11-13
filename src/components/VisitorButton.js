67631ee33f3acbf045339c3def96e86b70a448d4
Felixyaaaaaaa <felixyulianasterino@gmail.com>
2025-11-14 00:52:00 +0700

    update button

--- src/components/VisitorButton.js
+++ src/components/VisitorButton.js
@@ -5,7 +5,7 @@ import { FaEye } from "react-icons/fa";
 function VisitorButton({ count }) {
   return (
     <button
-      className="fixed bottom-4 left-4 flex items-center gap-2 bg-green-700 text-white font-semibold py-2 px-4 rounded-full shadow-lg hover:bg-green-800 transition-all duration-300"
+      className="fixed bottom-4 left-4 flex items-center gap-2 bg-green-700 text-white text-sm font-semibold py-2 px-4 rounded-full shadow-lg hover:bg-green-800 transition-all duration-300"
       style={{ zIndex: 1000 }}
     >
       <FaEye className="text-white text-lg" /> 
